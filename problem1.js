f94cf4627b955578ca5a64f0135a641f03d1e800
unknown <gt9675tiwari@gmail.com>
2022-09-13 21:47:43 +0530

    Fix Error

--- problem1.js
+++ problem1.js
@@ -1,13 +1,13 @@
 //num is prime or not 
 let num = 11 ; 
 let count = 0 ;
-for( let i=0 ; i<num ; i++ ){
+for( let i=1 ; i<=num ; i++ ){
 
   if( num%i===0 ){
     count++ ;
   }
 }
-if( count==1 ){
+if( count==2 ){
   console.log(num , "is prime ") ;
 }else {
   console.log(num , "is not prime ") ;
--- problem2.js
+++ problem2.js
@@ -1,7 +1,7 @@
 //check palindrome or not
 let str = "shoot" ;
 let rev = "" ;
-for( let i= str.length-1 ; i>=0 ; i++ ){
-  rev += str[i+1] ;
+for( let i= str.length-1 ; i>=0 ; i-- ){
+  rev += str[i] ;
 }
 console.log(str===rev) ;
